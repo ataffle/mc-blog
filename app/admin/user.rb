3556402ff4fe3c927ee04b5f2bb5cab8c151cdd6
Alix Taffle <ataffle@gmail.com>
2020-05-27 23:24:54 +0200

    complete admin set-up

---
+++ app/admin/user.rb
@@ -0,0 +1,32 @@
+ActiveAdmin.register User do
+
+  index do
+      selectable_column
+      column :id
+      column :email
+      column :full_name
+      column :title
+      column :admin
+      column :author
+      column :created_at
+      actions
+  end
+
+  form do |f|
+    f.inputs "Sign up" do
+      f.input :email
+      # f.input :password
+    end
+    f.inputs "Details" do
+      f.input :full_name
+      f.input :title
+    end
+    f.inputs "Access" do
+      f.input :admin
+      f.input :author
+    end
+    f.actions
+  end
+
+  permit_params :email, :full_name, :admin, :author, :title
+end
