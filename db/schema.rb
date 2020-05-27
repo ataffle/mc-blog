da18975bed2842b46e16d8156adc2c5833b12bac
Alix Taffle <ataffle@gmail.com>
2020-05-27 22:20:56 +0200

    set up Active Admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_140106) do
+ActiveRecord::Schema.define(version: 2020_05_27_200548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.string "author_type"
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -41,7 +55,7 @@ ActiveRecord::Schema.define(version: 2020_05_27_140106) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "full_name"
-    t.boolean "admin", default: false, null: false
+    t.boolean "admin", default: false
     t.boolean "author"
     t.string "title"
     t.index ["email"], name: "index_users_on_email", unique: true
