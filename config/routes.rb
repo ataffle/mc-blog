91b9e56c1dc8be489ffd795a14e8256ce17158c5
Alix Taffle <ataffle@gmail.com>
2020-06-02 18:40:42 +0200

    add show for users and categories

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   get 'posts/index'
   get 'categories/index'
+  get "categories/:id", to: "categories#show", as: :category
+  get "posts/:id", to: "posts#show", as: :post
+  get "users/:id", to: "users#show", as: :author
   devise_for :users
+  # resources :users do
+  #   resources :posts, only: [:show]
+  # end
   root to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
