dfcec797fcbb36087cc9fa21b91d7de8f8fc96d2
Alix Taffle <ataffle@gmail.com>
2020-07-21 16:49:47 +0200

    improve post show with more links

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ApplicationRecord
-  include PgSearch::Model
   belongs_to :category
   belongs_to :user
   default_scope { order(updated_at: :desc) }
   has_one_attached :photo
+
+  include PgSearch::Model
   pg_search_scope :global_search,
     against: { title: 'A', content: 'B' },
     associated_against: { user: [:full_name], category: [:title] },
